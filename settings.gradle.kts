8bea45df26edd823fcbff127a21401d3954656f6
vincenthu <519832638@qq.com>
2023-12-25 16:56:36 +0800

    Feather:project init

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "LiveRoom"
+include(":app")
+include(":LiveRoomLibs")
